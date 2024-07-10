84cfa2134e3bb9f2004d020ec3e7aea1ec6f8ec3
Shayn Voon <voonshayn@gmail.com>
2024-07-10 09:51:56 -0800

    small details

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ from datetime import datetime
 app = Flask(__name__, static_folder='static')
 app.secret_key = 'supersecretkey'
 
+#CONNECTION STRING for connection to Database
 conn_str = (
     'Driver={ODBC Driver 18 for SQL Server};'
     'Server=tcp:cybersecurityappserver.database.windows.net,1433;'
-    'Database=tccsqldbdemo;'
+    'Database=cybersecuritydemodb;'
     'UID=jaygokul;'
     'PWD=S3@ttleAzure$Float;'
     'Encrypt=yes;'
@@ -17,6 +18,7 @@ conn_str = (
     'Connection Timeout=30;'
 )
 
+# GET CONNECTION FOR DATABASE OPERATIONS
 def get_db_connection():
     try:
         conn = pyodbc.connect(conn_str)
