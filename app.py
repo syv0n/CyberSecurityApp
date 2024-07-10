49cf0c7200400f1ea670665316e9a4664ee4841c
Shayn Voon <voonshayn@gmail.com>
2024-07-10 09:42:58 -0800

    start app with connection string

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, render_template, redirect, url_for, flash, jsonify
+import pyodbc
+import os
+from datetime import datetime
+
+app = Flask(__name__, static_folder='static')
+app.secret_key = 'supersecretkey'
+
+conn_str = (
+    'Driver={ODBC Driver 18 for SQL Server};'
+    'Server=tcp:cybersecurityappserver.database.windows.net,1433;'
+    'Database=tccsqldbdemo;'
+    'UID=jaygokul;'
+    'PWD=S3@ttleAzure$Float;'
+    'Encrypt=yes;'
+    'TrustServerCertificate=yes;'
+    'Connection Timeout=30;'
+)
+
+def get_db_connection():
+    try:
+        conn = pyodbc.connect(conn_str)
+        print("Connection successful")
+        return conn
+    except pyodbc.Error as ex:
+        sqlstate = ex.args[0]
+        if sqlstate == '08001':
+            print("Network-related or instance-specific error occurred")
+        elif sqlstate == '28000':
+            print("Invalid authorization specification")
+        else:
+            print(f"Error: {ex}")
+        raise
