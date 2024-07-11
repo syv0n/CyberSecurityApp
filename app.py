f0639aa81913397066caafeac6cc5293b7569e4f
Shayn Voon <voonshayn@gmail.com>
2024-07-11 09:06:21 -0800

    small changes

--- app.py
+++ app.py
@@ -82,4 +82,9 @@ def insert_respond_RS_AN(data):
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('Nist_2.0.html')
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
